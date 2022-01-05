b58de3b6eb6e4bafe362542bbf4d767387471be5
madhav2928 <madhav2928@gmail.com>
2022-01-05 13:47:34 +0530

    Completed 14 th problem

---
+++ 1-Milestone_Goldman_sachs/14_minimum_size_subarraY_sum.cpp
@@ -0,0 +1,53 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int minSubArrayLen(int target, vector<int>& nums) {
+        int sum = 0;
+        int n = nums.size();
+        int i=0;
+        int j = 0;
+        int res = INT_MAX;
+        while(j<n)
+        {
+            sum = sum +nums[j];
+            if(sum>=target)
+            {
+                res = min(res,j-i+1);
+                while(sum>=target)
+                {
+                    sum = sum -nums[i];                    
+                    i++;
+                    if(sum>=target)
+                    {
+                        res = min(res,j-i+1);
+                    }
+                }
+            }
+            j++;
+        }
+        if(res>n)
+        {
+            return 0;
+        }
+        return res;
+    }
+};
+
+
+
+
+
+
+int main(){
+    int T;
+    cin>>T;
+    Solution s;
+    while(T--){
+        int t = 7;
+        vector<int> a = {2,3,1,2,4,3};
+        cout<<s.minSubArrayLen(t,a)<<"\n";
+    }
+}
